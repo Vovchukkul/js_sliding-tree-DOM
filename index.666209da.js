5635b6a7bcf167a74bc7356f9c82b25efaf97b81
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-08 16:40:14 +0200

    make build

---
+++ index.666209da.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-useless-return */var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var t=document.createElement("span");t.className="tree__item",e.prepend(t),t.append(t.nextSibling)}),e.addEventListener("click",function(e){if("tree__item"===e.target.className){var t=e.target.parentNode.children;if(t.length){var n=t[1];n.hidden=!n.hidden}}});//# sourceMappingURL=index.666209da.js.map
+
+//# sourceMappingURL=index.666209da.js.map
